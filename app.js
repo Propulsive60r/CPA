8bb6f4df8aadda06981a2ca075c01050f96309fc
Propulsive60r <Propulsive60r@yandex.ru>
2022-07-11 22:11:40 +0300

    add ООП v0.31

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// Create templates
+const colorTemplate = [
+    'color: red',
+    'color: green',
+    'color: blue',
+    'color: black',
+];
+
+const fontSizeTemplate = [
+    'font-size: 16px',
+    'font-size: 24px',
+    'font-size: 32px',
+];
+
+const marginTemplate = [
+    '5px',
+    '10px',
+    '15px',
+];
+
+//=========================================
 //Class DOM - layout
 class Layout {
     constructor(div) {
@@ -5,114 +26,78 @@ class Layout {
         document.body.append(this.layout);
     }
 }
-
-
+//=========================================
 //Header Layout
 const header = new Layout('header');
 header.layout.innerHTML = 'Header';
 
+//=========================================
 //Main Layout
 const main = new Layout('main');
-main.layout.innerHTML = 'Main';
+//main.layout.innerHTML = 'Main';
 
+//=========================================
 //Footer Layout
 const footer = new Layout('footer');
 footer.layout.innerHTML = 'Footer';
 
 
-
-
-main.layout.insertAdjacentHTML('afterend', '<h1>Article</h1>')
-
-
-
-
-
-
-
-// // Create templates
-// const colorTemplate = [
-//     'color: red',
-//     'color: green',
-//     'color: blue',
-//     'color: black',
-// ];
-//
-// const fontSizeTemplate = [
-//     'font-size: 16px',
-//     'font-size: 24px',
-//     'font-size: 32px',
-// ];
-//
-// const marginTemplate = [
-//     '5px',
-//     '10px',
-//     '15px',
-// ];
-//
-// //header
-// const $entry = document.querySelector('#entry');
-// $entry.innerHTML = 'Entry point / Header';
 // $entry.style.cssText = `
 // ${colorTemplate[0]};
 // ${fontSizeTemplate[0]}
 // `;
-// document.body.prepend($entry);
-//
-//
-// // general block class
-// class BlockInput {
-//     // price;
-//     // amount;
-//
-//     constructor() {
-//         this.placeholder = ['Price coins', 'Amount coin'];
-//         // this.price = price;
-//         // this.amount = amount;
-//         this.blockDiv = document.createElement('div');
-//
-//
-//         this.elementInputePrice = document.createElement('input');
-//         this.elementInputAmount = document.createElement('input');
-//         this.elementBlockAdd = document.createElement('span');
-//         this.elementBlockDelete = document.createElement('span');
-//
-//         this.elementInputePrice.placeholder = this.placeholder[0];
-//         this.elementInputAmount.placeholder = this.placeholder[1];
-//         this.elementBlockAdd.innerHTML = 'add';
-//         this.elementBlockDelete.innerHTML = ' delete';
-//         this.elementBlockAdd.id = 'addBlock';
-//         this.elementBlockDelete.id = 'deleteBlock';
-//
-//
-//         this.blockDiv.insertAdjacentElement('beforeend', this.elementInputePrice);
-//         this.blockDiv.insertAdjacentElement('beforeend', this.elementInputAmount);
-//         this.blockDiv.insertAdjacentElement('beforeend', this.elementBlockAdd);
-//         this.blockDiv.insertAdjacentElement('beforeend', this.elementBlockDelete);
-//
-//
-//         //this.arr = [this.elementInputePrice, this.elementInputAmount, this.elementBlockAdd, this.elementBlockDelete];
-//     };
-//
-//     createBlockInput() {
-// //const copyBlock = this.blockDiv.cloneNode();
-//
-//
-//
-//
-//         //this.arr.forEach(e => document.body.append(e));
-//     };
-//
-//     deleteBlockInput() {
-//         this.blockDiv.remove();
-//     }
-//
-// }
-//
-// const block1 = new BlockInput();
-//
-//
-//
+
+//=========================================
+// general block class
+class BlockInput {
+    // price;
+    // amount;
+
+    constructor() {
+        this.placeholder = ['Price coins', 'Amount coin'];
+        // this.price = price;
+        // this.amount = amount;
+        this.elementInputePrice = document.createElement('input');
+        this.elementInputAmount = document.createElement('input');
+        this.elementBlockAdd = document.createElement('span');
+        this.elementBlockDelete = document.createElement('span');
+
+        this.elementInputePrice.placeholder = this.placeholder[0];
+        this.elementInputAmount.placeholder = this.placeholder[1];
+        this.elementBlockAdd.innerHTML = 'add';
+        this.elementBlockDelete.innerHTML = ' delete';
+        this.elementBlockAdd.id = 'addBlock';
+        this.elementBlockDelete.id = 'deleteBlock';
+
+        // main.layout.insertAdjacentElement('beforeend', this.elementInputePrice);
+        // main.layout.insertAdjacentElement('beforeend', this.elementInputAmount);
+        // main.layout.insertAdjacentElement('beforeend', this.elementBlockAdd);
+        // main.layout.insertAdjacentElement('beforeend', this.elementBlockDelete);
+
+        this.arr = [this.elementInputePrice, this.elementInputAmount, this.elementBlockAdd, this.elementBlockDelete];
+        this.arr.forEach(e => main.layout.insertAdjacentElement('beforeend', e));
+
+    };
+
+    createBlockInput() {
+        this.arr.forEach(e => main.layout.insertAdjacentElement('beforeend', e));
+    };
+
+    deleteBlockInput() {
+        this.blockDiv.remove();
+    }
+
+}
+
+const block1 = new BlockInput();
+
+block1.elementBlockAdd.addEventListener('click', () => {
+    //block1.createBlockInput();
+   const block2 = new BlockInput()
+    block2.elementInputePrice.style.background = 'green'
+})
+
+
 // //block1.createBlockInput();
 // //  block1.elementBlockAdd.addEventListener('click', () => {
 // //
@@ -122,9 +107,3 @@ main.layout.insertAdjacentHTML('afterend', '<h1>Article</h1>')
 // //     //const block2 = new BlockInput();
 // //     block1.deleteBlockInput();
 // // })
-//
-//
-//
-//
-// //footer
-// document.body.insertAdjacentHTML('afterend', '<div>Footer</div>');
